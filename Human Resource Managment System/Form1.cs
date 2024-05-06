b5d9ce4d9eb51e9f0ab527f041ff9d60f07174bd
Conquerorr0 <fatihaltuntas0@outlook.com>
2024-05-06 23:21:40 +0300

    Giriş ekranı oluşturuldu ve gerekli komponentler ile arkaplan resmi eklendi.

--- Human Resource Managment System/Form1.cs
+++ Human Resource Managment System/Form1.cs
@@ -4,17 +4,45 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Human_Resource_Managment_System
 {
-    public partial class Form1 : Form
+
+    // Kullanıcı adı label -> lblUserName
+    // Şifre label -> lblPassword
+    // Kullanıcı adı textbox -> txtUserName
+    // Şifre textbox -> txtPassword (Şifre textbox'ına gizleme ve gösterme özelliği eklenebilir.
+    // Giriş yap butonu -> btnEnter
+
+    public partial class windowSıgnIn : Form
     {
-        public Form1()
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_MAXIMIZE = 3;
+        public windowSıgnIn()
         {
             InitializeComponent();
+            this.Load += new EventHandler(FullScreenExample_Load);
+        }
+
+        private void FullScreenExample_Load(object sender, EventArgs e)
+        {
+            // Formun boyutunu tam ekran yapmak için
+            this.WindowState = FormWindowState.Maximized;
+
+            // Tarayıcıyı tam ekran yapmak için
+            IntPtr hWnd = this.Handle;
+            ShowWindow(hWnd, SW_MAXIMIZE);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Giris Yapildi!");
         }
     }
 }
