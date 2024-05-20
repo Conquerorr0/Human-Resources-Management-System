102e35d8f48561e51f95d5eae3a8ff5de8c11688
Conquerorr0 <fatihaltuntas0@outlook.com>
2024-05-20 19:32:54 +0300

    Evindar Üçderenin yazmış olduğu katmanlı mimari backend kodunu kendi bilgisayarında commit atmada bir problem çıktığı için başka bir bilgisayardan (Fatih Altuntaş) deniyoruz!

---
+++ Entities/Models/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public abstract class BaseEntity
+    {//her tabloda olmasını istedigimiz özellikleri tanımlıyoruz 
+        public int tc { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; } 
+       
+        //yaratılma güncellenem ve silnme tarıhlerini de alabiliriz veya verinni hangi durumda oldugunu da 
+        protected BaseEntity()
+        {
+            // bu sınıf kalıtım olarak alındıgında gostermesini veya iletmesni 
+            //istedigimiz şeyleri iletecek 
+        }
+    }
+}
---
+++ Human Resource Managment System/Form2.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Human_Resource_Managment_System
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPersonle = new System.Windows.Forms.Button();
+            this.btnYonetim = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPersonle
+            // 
+            this.btnPersonle.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnPersonle.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPersonle.Location = new System.Drawing.Point(642, 320);
+            this.btnPersonle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPersonle.Name = "btnPersonle";
+            this.btnPersonle.Size = new System.Drawing.Size(259, 73);
+            this.btnPersonle.TabIndex = 0;
+            this.btnPersonle.Text = "Pesonel Girişi";
+            this.btnPersonle.UseVisualStyleBackColor = false;
+            this.btnPersonle.Click += new System.EventHandler(this.btnPersonle_Click);
+            // 
+            // btnYonetim
+            // 
+            this.btnYonetim.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnYonetim.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYonetim.Location = new System.Drawing.Point(642, 432);
+            this.btnYonetim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnYonetim.Name = "btnYonetim";
+            this.btnYonetim.Size = new System.Drawing.Size(259, 76);
+            this.btnYonetim.TabIndex = 1;
+            this.btnYonetim.Text = "Yönetim Girişi";
+            this.btnYonetim.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(659, 243);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.MaximumSize = new System.Drawing.Size(625, 625);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hoş Geldiniz";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Menu;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(642, 552);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ÇIKIŞ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1374, 727);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnYonetim);
+            this.Controls.Add(this.btnPersonle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPersonle;
+        private System.Windows.Forms.Button btnYonetim;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
