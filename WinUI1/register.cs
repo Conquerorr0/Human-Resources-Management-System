88b1b7b033f78f39d04197c53bd68431200e21d2
Mehmet Nur Kavan <kavanmehmetnur@gmail.com>
2024-06-01 19:01:52 +0300

    form 3 ve regıster

---
+++ WinUI1/register.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    internal class recourse
+    {
+        //Create a class for register... using this .
+        public string username { get; set; }
+        public string id { get; set; }
+        public string message { get; set; }
+        public string email{ get; set; }
+        public string phoneNumber { get; set; }
+
+
+    }
+}
