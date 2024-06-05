5512abc56406d52739d01db2e5883e5ed6e4b752
Conquerorr0 <fatihaltuntas0@outlook.com>
2024-06-05 15:34:43 +0300

    Hatalar giderildi!

--- WinUI1/Properties/Resources.Designer.cs
+++ WinUI1/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace WinUI1.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap show {
-            get {
-                object obj = ResourceManager.GetObject("show", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
